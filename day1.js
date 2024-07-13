4cd1d77e7774a6eb134289df313125d37295c4ee
Mohammad Ahmad <104555382+ahmadparizaad@users.noreply.github.com>
2024-07-13 22:13:50 +0530

    day1 variables and data types

---
+++ day1.js
@@ -0,0 +1,24 @@
+//Variables and Data Types
+var num = 10;
+console.log(num);
+console.log(typeof num);
+
+let str = "Hello Ahmad";
+console.log(str);
+console.log(typeof str);
+
+const bool = true;
+console.log(bool);
+console.log(typeof bool);
+
+const obj = {1:"a", 2:"b", 3:"c", 4:"d", 5:"e"};
+console.log(typeof obj);
+
+const arr = [1,2,3,4,5];
+console.log(typeof arr);
+
+str = "12";
+console.log(str);
+
+bool = false;
+console.log(bool);
